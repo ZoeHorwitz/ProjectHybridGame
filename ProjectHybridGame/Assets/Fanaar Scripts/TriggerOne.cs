f7c6da72cb0035c8c2591d22af518c349b62d7df
Fanaar Rashid <fanaarrashid@gmail.com>
2023-01-17 15:21:04 +0100

    Final Level

--- ProjectHybridGame/Assets/Fanaar Scripts/TriggerOne.cs
+++ ProjectHybridGame/Assets/Fanaar Scripts/TriggerOne.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TriggerOne : MonoBehaviour
 {
     public GameObject Door1;
+    public GameObject Flower1;
     public AudioSource Doors;
 
      void OnTriggerEnter2D(Collider2D other)
@@ -12,6 +13,7 @@ public class TriggerOne : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             Door1.SetActive(false);
+            Flower1.SetActive(false);
             Doors.Play();
             Debug.Log("Test!");
         }
--- ProjectHybridGame/Assets/Fanaar Scripts/TriggerTwo.cs
+++ ProjectHybridGame/Assets/Fanaar Scripts/TriggerTwo.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class TriggerTwo : MonoBehaviour
 {
     public GameObject Door2;
+    public GameObject Flower2;
     public AudioSource Doors;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
             Door2.SetActive(false);
+            Flower2.SetActive(false);
             Debug.Log("Test!");
             Doors.Play();
         }
